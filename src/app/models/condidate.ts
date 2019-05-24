0715fc5870aba91efb395a6e0c0ef1ae15f75317
khalfallah.bilel@gmail.com <khalfallah.bilel@gmail.com>
2019-05-24 15:02:02 +0200

    ok

---
+++ src/app/models/condidate.ts
@@ -0,0 +1,11 @@
+export class Condidate{
+    firstName:string;
+    lastName:string;
+    spec:string;
+    age:string;
+    mob:string;
+    address:string;
+    email:string;
+    link:string;
+    about:string;
+}
