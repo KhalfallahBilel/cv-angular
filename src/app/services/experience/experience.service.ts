978b883c0cd680a8e13bc3240747022d5c56150d
khalfallah.bilel@gmail.com <khalfallah.bilel@gmail.com>
2019-05-29 16:49:40 +0200

    commit

--- src/app/services/experience/experience.service.ts
+++ src/app/services/experience/experience.service.ts
@@ -15,27 +15,35 @@ export class ExperienceService {
     },
     {
       id: 1,
-      date: '2015-2018',
-      companyName: 'PA Concept– Mougins',
-      poste:"Technicien de revêtement Piscines",
+      date: '2018',
+      companyName: 'SII - Sophia',
+      poste:"Développeur web fullStack",
       description:"Manœuvre en bâtiment de construction des piscines. Technicien de revêtement des piscines avec des produits composites (Résine)",
-      technos:" "
+      technos:"ANGULAR 5 BOOTSTRAP 4 TYPESCRIPT JAVASCRIPT HTML5, CSS3 JAVA, JEE HIBERNATE, JERSEY, SQL MAVEN"
     },
     {
       id: 2,
-      date: '2013-2015',
-      companyName: 'CUISTO– Tunisie',
-      poste:"WebMaster",
-      description:"gjf",
-      technos:"cqscqs"
+      date: '2018',
+      companyName: 'SOFTEAM INSTITUTE– Mougins',
+      poste:"Développeur Stagiaire FullStack JAVA / JEE - Angular",
+      description:"Manœuvre en bâtiment de construction des piscines. Technicien de revêtement des piscines avec des produits composites (Résine)",
+      technos:"ANGULAR 5 BOOTSTRAP 4 TYPESCRIPT JAVASCRIPT HTML5, CSS3 JAVA, JEE HIBERNATE, JERSEY, SQL MAVEN"
     },
     {
       id: 3,
-      date: '2011/2012',
-      companyName: 'aaa',
-      poste:"nnnnn",
+      date: '2013-2015',
+      companyName: 'CUISTO– Tunisie',
+      poste:"Webmaster - infographiste",
       description:"gjf",
-      technos:"cqscqs"
+      technos:"HTML5, CSS3, JAVASCRIPT, PHP FILEZILLA, GOOGLE ANALYTICS ADOBE PHOTOSHOP, ILLUSTRATOR, INDESIGN, PREMIERE, AUDITION"
+    },
+    {
+      id: 4,
+      date: '2012/2013',
+      companyName: 'CPM – TUNISIE',
+      poste:"Responsable Technique",
+      description:"gjf",
+      technos:"HTML5, CSS3 JAVASCRIPT PHP, SQL PHOTOSHOP, ILLUSTRATOR, COREL DRAW, FTP SERVER, FILEZILLA, GOOGLEANALYTICS"
     }
   ];
   constructor() { }
