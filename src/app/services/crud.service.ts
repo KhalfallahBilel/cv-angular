43b81511c478eb4123d5dd5039503d040187cf51
khalfallah.bilel@gmail.com <khalfallah.bilel@gmail.com>
2019-06-03 14:36:02 +0200

    generic crud service

---
+++ src/app/services/crud.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CrudOperations } from '../models/crud-operations.interface';
+import { HttpClient, HttpHeaders } from '@angular/common/http/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService<T,ID> implements CrudOperations<T,ID> {
+
+  constructor(
+    protected _http: HttpClient,
+    protected _base: string
+  ) { }
+
+  httpOptions = {
+    headers: new HttpHeaders().set('Authorization', `Bearer `+localStorage.getItem('tokenf')),
+    header: new HttpHeaders().set('Content-Type', 'multipart/form-data')
+  };
+
+  save(payload: T): Observable<T> {
+    return this._http.post<T>(this._base, payload,this.httpOptions);
+  }
+
+  update(id: ID, payload: T): Observable<T> {
+    return this._http.put<T>(this._base + "/" + id, payload,this.httpOptions);
+  }
+
+  findOne(id: ID): Observable<T> {
+    return this._http.get<T>(this._base + "/" + id,this.httpOptions);
+  }
+
+  findAll(): Observable<T[]> {
+    return this._http.get<T[]>(this._base+'/list' ,this.httpOptions)
+  }
+
+  delete(id: ID): Observable<T> {
+    return this._http.delete<T>(this._base + '/' + id,this.httpOptions);
+  }
+  
+  findAllByManager(id:ID): Observable<T[]> {
+    return this._http.get<T[]>(this._base+'user/' + id + '/project',this.httpOptions)
+  }
+}
